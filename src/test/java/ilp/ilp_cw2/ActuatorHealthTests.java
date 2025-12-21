2c4743d894d4af8bb2288bb5dcea2e16c06c325d
Reese <eseeria@gmail.com>
2025-12-21 22:44:02 +0000

    Adding a purposfully failing test to test the CI/badge

--- src/test/java/ilp/ilp_cw2/ActuatorHealthTests.java
+++ src/test/java/ilp/ilp_cw2/ActuatorHealthTests.java
@@ -18,4 +18,9 @@ public class ActuatorHealthTests extends TestTemplate {
     void CorrectResponse_ValidGet_ReturnsStatus200() {
         assert(restTemplate.getForEntity(url, String.class).getStatusCode().value() == 200);
     }
+
+    @Test
+    void Purposfully_Failing_Test() {
+        assert(false);
+    }
 }
